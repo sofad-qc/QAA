e51febb537c90dcbc36a554e8d3ac3ca2b5ed018
François Tessier <francoistessier@Francois-Tessiers-MacBook-Air.local>
2025-11-06 10:29:54 -0500

    Test script branche

--- paresseux.sh
+++ paresseux.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
+# Affiche le statut Git
 git status
+
+# Ajoute tous les fichiers modifiés
 git add .
+
+# Demande le message de commit
 read -p "Entrez le message de commit : " commit_message
+
+# Effectue le commit
 git commit -m "$commit_message"
-git push origin main
+
+# Demande la branche à pousser
+read -p "Entrez le nom de la branche à pousser (par défaut : main) : " branch_name
+
+# Si l'utilisateur ne saisit rien, utiliser 'main'
+branch_name=${branch_name:-main}
+
+# Pousse sur la branche choisie
+git push origin "$branch_name"
